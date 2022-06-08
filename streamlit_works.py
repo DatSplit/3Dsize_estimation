5ec30aa143303d5ea0d732e5ecf39ed1b6791e0a
DatSplit <68016450+DatSplit@users.noreply.github.com>
2022-06-08 15:58:16 +0200

    Update docs

--- streamlit_works.py
+++ streamlit_works.py
@@ -5,35 +5,34 @@ import cv2
 from PIL import Image
 
 
-logo2 = cv2.imread('dlier.png').all()
-print((logo2))
-st.set_page_config(page_icon = logo2,page_title="3D size estimation" )
 
-# streamlit run C:\Users\niels\PycharmProjects\streamlit_test\streamlit_works.py
+# Read logo
 logo = cv2.imread('datalier_logo.png')
 logo = cv2.cvtColor(logo,cv2.COLOR_BGR2RGB)
 
+# Display logo
 st.image(logo)
 
 st.subheader('Object identification and dimension measurements.',anchor="Object identification and dimension measurements.")
-#s
+
+# Add selection box for objects
 option = st.selectbox(
 'Which object would you like to identify and measure?',
 ('Cup', 'Chair', 'Shoe', 'Camera'))
 
-
+# Add number input box
 distance = st.number_input('What is the distance to the object in (mm)?')
 if(distance == 0):
     st.error('Please enter a number.')
-#x_width = st.number_input('Please enter the width of the image in pixels.')
-#y_height = st.number_input('Please enter the height of the image in pixels.')
-#distance = 300 #mm
+
+# Camera parameters of image
 sensor_height_y = 3.6 #mm
 focal_length_camera = 2.65 #mm
 sensor_height_x = 3.6
 x_width = 1280
 y_height = 720
 
+# Add box to upload image to be identified and measured
 image2 = st.file_uploader('Upload an image containing a ' + str(option), help="Upload image containing a Cup with size", type=['png','jpg'])
 
 
@@ -69,7 +68,7 @@ with mp_objectron.Objectron(static_image_mode=True,
             print(f'No box landmarks detected')
             st.write('No ' + str(option) + ' detected.')
         else:
-            #print(f'Box landmarks detected')
+
             annotated_image = img_array.copy()
 
 
@@ -78,19 +77,22 @@ with mp_objectron.Objectron(static_image_mode=True,
               mp_drawing.draw_landmarks(
                   annotated_image, detected_object.landmarks_2d, mp_objectron.BOX_CONNECTIONS)
 
+
+
               if(str(option) == "Cup" or "Camera" or "Chair"):
 
-
+                  # height,length, and width calculation of the object (Cup, Camera, or Chair).
+                  # See the jupyter notebooks for explanations of the calculations.
                   height_shoe = abs(detected_object.landmarks_2d.landmark[2].y - detected_object.landmarks_2d.landmark[4].y)
                   height_shoe_pixels = height_shoe * y_height
                   height_shoe_cm = ((distance * height_shoe_pixels * sensor_height_y) / (focal_length_camera * y_height))
-                  print('h', height_shoe_cm)
+
                   pixels_per_metric_y = height_shoe_pixels / height_shoe_cm
 
                   length_shoe = abs(detected_object.landmarks_2d.landmark[2].x - detected_object.landmarks_2d.landmark[6].x)
                   length_shoe_pixels = length_shoe * x_width
                   length_shoe_cm = ((distance * length_shoe_pixels * sensor_height_x) / (focal_length_camera * x_width))
-                  print(length_shoe_cm)
+
 
                   width_shoe = abs(detected_object.landmarks_2d.landmark[2].y - detected_object.landmarks_2d.landmark[1].y)
 
@@ -98,19 +100,22 @@ with mp_objectron.Objectron(static_image_mode=True,
 
               if(str(option) == "Shoe"):
 
+
+                  # Different landmarks are required for the length, width, and height of the shoe, due to the natural orientation of the shoe.
+                  # See the jupyter notebooks for explanations of the calculations.
                   height_shoe = abs(
                       detected_object.landmarks_2d.landmark[5].y - detected_object.landmarks_2d.landmark[7].y)
                   height_shoe_pixels = height_shoe * y_height
                   height_shoe_cm = (
                               (distance * height_shoe_pixels * sensor_height_y) / (focal_length_camera * y_height))
-                  print('h', height_shoe_cm)
+
                   pixels_per_metric_y = height_shoe_pixels / height_shoe_cm
 
                   length_shoe = abs(
                       detected_object.landmarks_2d.landmark[6].x - detected_object.landmarks_2d.landmark[7].x)
                   length_shoe_pixels = length_shoe * x_width
                   length_shoe_cm = ((distance * length_shoe_pixels * sensor_height_x) / (focal_length_camera * x_width))
-                  print(length_shoe_cm)
+
 
                   width_shoe = abs(
                       detected_object.landmarks_2d.landmark[2].y - detected_object.landmarks_2d.landmark[6].y)
@@ -126,7 +131,7 @@ with mp_objectron.Objectron(static_image_mode=True,
               roi[np.where(mask)] = 0
               roi += logo
 
-
+              # Add text about the length, width, and height.
               cv2.putText(
                   annotated_image, "Lengte: " + "{:.1f}".format((length_shoe_cm/10)) +
                          ' cm' + "  " + "Breedte: " + "{:.1f}".format(
@@ -134,15 +139,11 @@ with mp_objectron.Objectron(static_image_mode=True,
                                              "  " + "Hoogte: " + "{:.1f}".format(
                       (height_shoe_cm/10)) + ' cm', (70,700), 2,
                   1.2, (255, 255, 255), 2)
-
+              # Display the annotated image
               st.image(annotated_image)
 
 
 
 
 
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-#st.write("insert image please!")
-#st.button("Re-run")
+
